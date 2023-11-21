e35f2691e4491bf457ceb8ede48aaf5661987350
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2023-11-21 15:44:10 +0100

    Added Files

---
+++ data.js
@@ -0,0 +1,59 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20231121_162618_00_004",
+      "name": "IMG_20231121_162618_00_004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.11968206474085,
+          "pitch": -0.20937169923154286,
+          "title": "What?",
+          "text": "Weird...."
+        },
+        {
+          "yaw": 2.538356992017535,
+          "pitch": -0.17492308340941776,
+          "title": "Criminal activity<br>",
+          "text": "Really weird<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
